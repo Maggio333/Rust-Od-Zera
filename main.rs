1fff078e75c52e519b266e2acb49c435eda1718f
Maggio333 <arkadiusz.slota03@gmail.com>
2026-01-07 05:15:48 +0100

    Force language detection update

---
+++ main.rs
@@ -0,0 +1,9 @@
+// Ten plik służy do poprawnego wykrywania Rust jako głównego języka na GitHubie
+// Większość kodu Rust znajduje się w katalogu examples/
+
+fn main() {
+    println!("🦀 Rust Od Zera - Projekt edukacyjny");
+    println!("Zobacz dokumentację w katalogach 01-podstawy/, 02-ownership/, itd.");
+    println!("Przykłady kodu znajdują się w katalogu examples/");
+}
+
